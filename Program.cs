fabf7104addeba81dfb4bbb8b92a79c8e91fb30d
gaganVirk <deep.virk@outlook.co.nz>
2019-07-29 20:39:46 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prim_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string r = "ggg";
+            Console.WriteLine(r);
+            Console.WriteLine("PRIM'S ALGORITHMS");
+        }
+    }
+}

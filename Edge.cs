aaf9a8e6eccf9cefa93b40e04434261f4a8b68b7
gaganVirk <deep.virk@outlook.co.nz>
2019-07-30 22:40:09 +1200

    Created Edge and Vertex class

---
+++ Edge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prim_Algorithm
+{
+    class Edge
+    {
+        public int Weight { get; set; }
+    }
+}
---
+++ Vertex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prim_Algorithm
+{
+    class Vertex
+    {
+        public int Key { get; set; }
+
+    }
+}

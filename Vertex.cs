fe7ba87d46672848d06b7c092dd198a50bf52de4
gaganVirk <deep.virk@outlook.co.nz>
2019-07-31 20:33:07 +1200

    updated vertex class

--- Vertex.cs
+++ Vertex.cs
@@ -8,7 +8,84 @@ namespace Prim_Algorithm
 {
     class Vertex
     {
-        public int Key { get; set; }
+        const int INFINITY = 9999999;
+        int id;
+        int key;
+        Vertex pred;
+        bool visited;
+
+        public Vertex(int id)
+        {
+            this.Id = id;
+            pred = null;
+            Key = INFINITY;
+            visited = false;
+        }
+
+        public List<Edge> adj = new List<Edge>();
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        internal Vertex Pred
+        {
+            get
+            {
+                return pred;
+            }
+
+            set
+            {
+                pred = value;
+            }
+        }
+
+        public int Key
+        {
+            get
+            {
+                return key;
+            }
+
+            set
+            {
+                key = value;
+            }
+        }
+
+        public bool Visited
+        {
+            get
+            {
+                return visited;
+            }
+
+            set
+            {
+                visited = value;
+            }
+        }
+
+        public string adjline()
+        {
+            string pp = pred == null ? "-" : pred.id.ToString();
+            string s = string.Format("{0}:{1}:{2}", this.id, pp, key);
+            foreach (Edge edge in adj)
+            {
+                s += " " + edge.Dst.id;
+            }
+            return s;
+        }
 
     }
 }
